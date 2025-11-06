bccba2e13e291ce3311481bef2b006becae2c5a5
MohammadSaum <mohdsaum14@gmail.com>
2025-11-06 15:40:22 +0530

    mock api called

--- src/assets/components/chatWindow/MessagingContact.jsx
+++ src/assets/components/chatWindow/MessagingContact.jsx
@@ -1,34 +1,59 @@
-import React from 'react'
+import axios from 'axios'
 import AdminProfile from '../sideBar/AdminProfile'
+import { useEffect, useState } from 'react'
 
 const MessagingContact = () => {
-  return (
-    <div className='messagingContact rounded-xl flex h-16 pl-0 p-3 items-center hover:bg-gray-700 cursor-pointer mb-2 w-full shrink-0 duration-200'>
-        <div className='w-15 h-13 flex rounded-full items-center mr-3 justify-center border border-gray-500'>
-            <AdminProfile/>
-        </div>
 
-        <div className='flex flex-col w-full min-w-0 justify-center'>
+    const [messageCard, setMessageCard] = useState([])
 
-            <div className='flex h-7 w-full items-center justify-between'>
-                <div className='truncate font-medium'>
-                    Saum
-                </div>
-                
-                <div className='duration shrink-0 text-xs'>
-                    Yesterday
-                </div>
-
-            </div>
-
-            {/* bottom */}
-            <div className='text-sm sent truncate'>
-                Lorem ipsum dolor sit amet conse Lorem ipsum dolor sit amet consectetur.
-            </div>
-        </div>
+    const data = async() =>{
+        try{
+            const res = await axios.get('https://randomuser.me/api/?results=10')  
+        setMessageCard(res.data.results)
+        } catch (error){
+            console.error('error occured: ', error)
+        }
         
-    </div>
-  )
+    }
+
+    useEffect(()=>{
+        data(),[]
+    })
+
+    let msgCardData;
+
+    msgCardData = messageCard.map((elem,idx)=>{
+        return
+    })
+
+    return (
+
+        <div className='messagingContact rounded-xl flex h-16 pl-0 p-3 items-center hover:bg-gray-700 cursor-pointer mb-2 w-full shrink-0 duration-200'>
+            <div className='w-15 h-13 flex rounded-full items-center mr-3 justify-center border border-gray-500'>
+                <AdminProfile/>
+            </div>
+
+            <div className='flex flex-col w-full min-w-0 justify-center'>
+
+                <div className='flex h-7 w-full items-center justify-between'>
+                    <div className='truncate font-medium'>
+                        Saum
+                    </div>
+                    
+                    <div className='duration shrink-0 text-xs'>
+                        Yesterday
+                    </div>
+
+                </div>
+
+                {/* bottom */}
+                <div className='text-sm sent truncate'>
+                    Lorem ipsum dolor sit amet conse Lorem ipsum dolor sit amet consectetur.
+                </div>
+            </div>
+            
+        </div>
+    )
 }
 
 export default MessagingContact
