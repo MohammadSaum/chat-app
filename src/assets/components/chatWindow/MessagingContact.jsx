a906cd81f839f42ac3d0b40e792551224dcc54d5
MohammadSaum <mohdsaum14@gmail.com>
2025-11-06 20:47:29 +0530

    message cards done

--- src/assets/components/chatWindow/MessagingContact.jsx
+++ src/assets/components/chatWindow/MessagingContact.jsx
@@ -1,59 +1,58 @@
 import axios from 'axios'
-import AdminProfile from '../sideBar/AdminProfile'
 import { useEffect, useState } from 'react'
 
-const MessagingContact = () => {
 
-    const [messageCard, setMessageCard] = useState([])
+const MessagingContactList = () => {
+  const [users, setUsers] = useState([])
 
-    const data = async() =>{
-        try{
-            const res = await axios.get('https://randomuser.me/api/?results=10')  
-        setMessageCard(res.data.results)
-        } catch (error){
-            console.error('error occured: ', error)
-        }
-        
+  useEffect(() => {
+    const data = async () => {
+      try {
+        const res = await axios.get('https://randomuser.me/api/?results=20')
+        setUsers(res.data.results)
+      } catch (error) {
+        console.error('error occurred:', error)
+      }
     }
+    data()
+  }, [])
 
-    useEffect(()=>{
-        data(),[]
-    })
+const MessagingContactCard = ({ user }) => {
 
-    let msgCardData;
+  return (
+    <div className='messagingContact rounded-xl flex h-16 pl-0 p-3 items-center hover:bg-gray-700 cursor-pointer mb-2 w-full shrink-0 duration-200'>
+      <div className='w-15 h-13 flex rounded-full items-center mr-3 justify-center'>
+        <img
+          src={user.picture.large}
+          alt={`${user.name.first} ${user.name.last}`}
+          className='rounded-full w-12 h-12 object-cover'
+        />
+      </div>
 
-    msgCardData = messageCard.map((elem,idx)=>{
-        return
-    })
-
-    return (
-
-        <div className='messagingContact rounded-xl flex h-16 pl-0 p-3 items-center hover:bg-gray-700 cursor-pointer mb-2 w-full shrink-0 duration-200'>
-            <div className='w-15 h-13 flex rounded-full items-center mr-3 justify-center border border-gray-500'>
-                <AdminProfile/>
-            </div>
-
-            <div className='flex flex-col w-full min-w-0 justify-center'>
-
-                <div className='flex h-7 w-full items-center justify-between'>
-                    <div className='truncate font-medium'>
-                        Saum
-                    </div>
-                    
-                    <div className='duration shrink-0 text-xs'>
-                        Yesterday
-                    </div>
-
-                </div>
-
-                {/* bottom */}
-                <div className='text-sm sent truncate'>
-                    Lorem ipsum dolor sit amet conse Lorem ipsum dolor sit amet consectetur.
-                </div>
-            </div>
-            
+      <div className='flex flex-col w-full min-w-0 justify-center'>
+        <div className='flex h-7 w-full items-center justify-between'>
+          <div className='truncate'>
+            {user.name.first} {user.name.last}
+          </div>
+          <div className='duration shrink-0 text-xs'>Yesterday</div>
         </div>
-    )
+
+        <div className='text-sm sent truncate'>
+          {'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, unde.'} 
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default MessagingContact
+
+  return (
+    <div>
+      {users.map((user, idx) => (
+        <MessagingContactCard key={idx} user={user} />
+      ))}
+    </div>
+  )
+}
+
+export default MessagingContactList
