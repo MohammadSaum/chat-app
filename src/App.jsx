d83b675fcc87edc658baea8418c877e768418d81
MohammadSaum <mohdsaum14@gmail.com>
2025-11-07 16:56:33 +0530

    changing mid section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import Sidebar from './assets/components/sideBar/SideBar'
-import ChatWindow from './assets/components/chatWindow/ChatPage/ChatWindow'
-import Default from './assets/components/ChatScreen/Default'
+import { useState } from 'react'
+import Sidebar from '../src/assets/components/sideBar/SideBar'
+import ChatPage from './assets/components/chatWindow/ChatPage/ChatWindow' 
+import StatusPage from './assets/components/chatWindow/StatusPage/StatusPage'
+import ChannelPage from './assets/components/chatWindow/ChannelPage/ChannelPage'
+import CommunityPage from './assets/components/chatWindow/CommunityPage/CommunityPage'
+import SettingPage from './assets/components/chatWindow/SettingPage/SettingPage'
+import AdminPage from './assets/components/chatWindow/AdminPage/AdminPage'
+
 
 const App = () => {
+
+    const [active, setActive] = useState('Chat')
+
   return (
     <div className='mainScreen shrink-0 overflow-y-auto h-screen w-full flex hide-scrollbar'>
-      <Sidebar />
-      <Default/>
+      <Sidebar active={active} onSelect={setActive} />
+      <main className="flex-1  bg-[#161717] overflow-auto">
+        {active === 'Chat' && <ChatPage />}
+        {active === 'Status' && <StatusPage />}
+        {active === 'Channel' && <ChannelPage />}
+        {active === 'Community' && <CommunityPage />}
+        {active === 'Setting' && <SettingPage />}
+        {active === 'AdminProfile' && <AdminPage />}
+      </main>
     </div>
   )
 }
--- src/assets/components/chatWindow/ChatPage/ChatWindow.jsx
+++ src/assets/components/chatWindow/ChatPage/ChatWindow.jsx
@@ -2,10 +2,11 @@ import Header from './Header'
 import SearchComponent from './SearchComponent'
 import SortComponent from './SortComponent'
 import MessagingContact from './MessagingContact'
+import Default from '../../ChatScreen/Default'
 
 const ChatWindow = () => {
   return (
-    <div className='flex flex-col shrink-0 overflow-auto border-r border-gray-500 h-screen w-md text-white font-semibold text-2xl p-5 pb-0'>
+    <div className=' flex flex-col shrink-0 overflow-auto border-r border-gray-500 h-screen w-md text-white font-semibold text-2xl p-5 pb-0'>
         <Header/>
         <SearchComponent/>
         <SortComponent/>
